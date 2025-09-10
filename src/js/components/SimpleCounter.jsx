0c406544a2ffb5080e88800d60b7b62df7113251
Elizabeth Duarte Ortiz <134437416+liv-ortiz25@users.noreply.github.com>
2025-09-10 21:36:15 +0000

    trabajo terminado

--- src/js/components/SimpleCounter.jsx
+++ src/js/components/SimpleCounter.jsx
@@ -1,4 +1,36 @@
+import React, { useState, useEffect } from "react";
 
-function Counter (){
-    
-};
+function SimpleCounter() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(prev => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  
+  const six = Math.floor(counter / 100000) % 10;
+  const five = Math.floor(counter / 10000) % 10;
+  const four = Math.floor(counter / 1000) % 10;
+  const three = Math.floor(counter / 100) % 10;
+  const two = Math.floor(counter / 10) % 10;
+  const one = counter % 10;
+
+  return (
+    <div className="container">
+      <div className="icon"><i className="far fa-clock"></i></div>
+      <div className="six">{six}</div>
+      <div className="five">{five}</div>
+      <div className="four">{four}</div>
+      <div className="three">{three}</div>
+      <div className="two">{two}</div>
+      <div className="one">{one}</div>
+    </div>
+  );
+}
+
+
+export default SimpleCounter;
